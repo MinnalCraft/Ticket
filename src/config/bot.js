c7d0161db7353787633ef0db6593f7727d5a7a1a
MinnalCraft <104630044+MinnalCraft@users.noreply.github.com>
2023-05-07 15:06:50 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-    token: "MTEwNDcwMTQxNDkwMDUxODk0Mg.GG2ori.bbPnMe7JJ1M9r655wCPe-vceq2226DZdgZSZb4",
-    prefix: "t!"
+    token: "",
+    prefix: ""
 }
