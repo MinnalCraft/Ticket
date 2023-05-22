0f2c5b2189b13769103161cf9dc6bb6a6ea7665f
MinnalCraft <104630044+MinnalCraft@users.noreply.github.com>
2023-05-22 18:58:11 +0530

    Update ready.js

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -3,7 +3,7 @@ const figlet = require("figlet");
 
 module.exports = async (client) => {
   client.Ready = true;
-  client.user.setActivity("THARAVAD MC", { type: "PLAYING", url:"https://discord.gg/DAkSpgH4rn" }).then(() => {
+  client.user.setActivity("TVD NETWORK", { type: "PLAYING", url:"https://discord.gg/uHB8xVKp" }).then(() => {
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag);
   });
